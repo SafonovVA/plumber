86168b1fb4e00258e8e7ab2336d38318325c66f3
Vladimir Rebo <vladimir.rebo@ffins.kz>
2022-07-07 12:15:59 +0600

    next

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -14,7 +14,7 @@
     <link href="https://fonts.googleapis.com/css?family=Montserrat:400,700" rel="stylesheet" type="text/css" />
     <link href="https://fonts.googleapis.com/css?family=Roboto+Slab:400,100,300,700" rel="stylesheet" type="text/css" />
     <!-- Core theme CSS (includes Bootstrap)-->
-    @vite(['resources/css/app.css', 'resources/js/app.js', 'resources/images/navbar-logo.svg'])
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
 </head>
 <body id="page-top">
 <!-- Navigation-->
